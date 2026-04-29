cd92ddff4c521b1736c934150511f6f2306488ce
Dimasprasetyo <dimasboy21345@gmail.com>
2026-04-29 10:21:15 +0700

    SEO
    ADD:library for heading metadata to docs use meta og tittle and description

---
+++ src/lib/seo.ts
@@ -0,0 +1,31 @@
+export function buildSeoMeta({
+    title,
+    description,
+    url,
+}: {
+    title?: string
+    description?: string
+    url?: string
+}) {
+    return {
+        meta: [
+            { title:
+                 title ?? 'Docs' 
+                },
+            {
+                name: 'description',
+                content: description || 'Dokumentasi lengkap'
+            },
+
+            { property: 'og:title', content: title },
+            { property: 'og:description', content: description },
+            { property: 'og:url', content: url },
+        ],
+        links: [
+            {
+                rel: 'canonical',
+                href: url,
+            },
+        ],
+    }
+}
