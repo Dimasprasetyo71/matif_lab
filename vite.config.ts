f5132ec39a23e4b096c3d3d1553e07ef04a87757
Dimasprasetyo <dimasboy21345@gmail.com>
2026-04-29 11:17:20 +0700

    Update vite.config.ts
    chore(vite): configure MDX plugin and SSR externals for Fumadocs

--- vite.config.ts
+++ vite.config.ts
@@ -2,22 +2,50 @@ import { defineConfig } from 'vite'
 import { devtools } from '@tanstack/devtools-vite'
 
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import mdx from 'fumadocs-mdx/vite'
+// import { nitro } from 'nitro/vite'
 
 const config = defineConfig({
-  resolve: { tsconfigPaths: true },
+  resolve: {
+    tsconfigPaths: true,
+    noExternal: [
+      'fumadocs-core',
+      'fumadocs-ui',
+      'fumadocs-openapi',
+      '@fumadocs/base-ui',
+    ],
+  },
   plugins: [
     devtools(),
     tailwindcss(),
-    tanstackStart(),
+
+    tanstackStart({
+      prerender: {
+        enabled: true,
+        crawlLinks: true,
+      },
+    }),
+    mdx(await import('./source.config')),
     viteReact({
       babel: {
         plugins: ['babel-plugin-react-compiler'],
       },
     }),
   ],
+  ssr: {
+    noExternal: [
+      'fumadocs-core',
+      'fumadocs-ui',
+      'fumadocs-openapi',
+      '@fumadocs/base-ui',
+    ],
+  },
+  build: {
+    target: 'esnext',
+    minify: 'esbuild',
+  },
 })
 
 export default config
