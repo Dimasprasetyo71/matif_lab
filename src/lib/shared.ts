1c06b868b106db3a2214c3998d52eb5cfe72e61e
Dimasprasetyo <dimasboy21345@gmail.com>
2026-04-29 11:22:10 +0700

    Create shared.ts
    chore(config): define docs routes and GitHub repository settings

---
+++ src/lib/shared.ts
@@ -0,0 +1,10 @@
+export const appName = 'Tanstack Start'
+export const docsRoute = '/docs'
+export const docsImageRoute = '/og/docs'
+export const docsContentRoute = '/llms.mdx/docs'
+
+export const gitConfig = {
+  user: 'Dimasprasetyo71',
+  repo: 'matif_lab',
+  branch: 'master',
+}
