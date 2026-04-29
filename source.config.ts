96bc5a8eba2a28c89045ea1b1664cc94bef58a9b
Dimasprasetyo <dimasboy21345@gmail.com>
2026-04-29 10:58:43 +0700

    fumadocs-config
    Config  fumadocs  using plugin remarth and rehypekatex to math and katex

---
+++ source.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig, defineDocs, metaSchema } from 'fumadocs-mdx/config'
+import remarkMath from 'remark-math'
+import rehypeKatex from 'rehype-katex'
+
+export const docs = defineDocs({
+  dir: 'content/docs',
+  docs: {
+    postprocess: {
+      includeProcessedMarkdown: true,
+    },
+
+    async: true,
+  },
+  meta:{
+    schema : metaSchema
+  }
+})
+
+export default defineConfig({
+  mdxOptions: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: (v) => [rehypeKatex, ...v],
+  },
+
+})
